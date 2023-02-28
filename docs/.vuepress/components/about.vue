af7fd06124363832a14d3e8e5c30b22bd134ed28
害羞小男孩哼 <stephen@haixiuxiaonanhais-MacBook-Pro.local>
2023-02-28 13:12:26 -0500

    feat: about page

--- docs/.vuepress/components/about.vue
+++ docs/.vuepress/components/about.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="about">
-    <div>I am Jiaqiang Li</div>
+  <div class="container about">
+    <div class="paragraph">Hi! I am Jiaqiang Li</div>
+    <div class="paragraph">I got my Bachelor's Degree at <span class="bold">Shandong University</span> in 2014. Then I joined <span class="bold">University of Toronto</span> and got a Master's Degree in Computer Engineering in 2016.</div>
+    <div class="paragraph">Now I am working at Shopee as a <span class="bold">Senior Front-End Engineer.</span></div>
+    <div>Feel free to contact with me by <span class="bold">qwnwzzo@163.com</span></div>
   </div>
 </template>
 
@@ -8,3 +11,15 @@
 export default {
 }
 </script>
+
+<style scoped>
+.container {
+  font-size: medium;
+}
+.paragraph {
+  margin-bottom: 10px;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
